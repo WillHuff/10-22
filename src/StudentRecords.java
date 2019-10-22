bacf108ba533f20ff594d952d2ad11528275ec3e
whuff <whuff@uh.edu>
2019-10-22 13:01:20 -0500

    Initial commit

---
+++ src/StudentRecords.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class StudentRecords {
+    public static void main(String[] args){
+        Scanner darkly=new Scanner(System.in);
+        System.out.println("How many students will be recorded?");
+        int n=darkly.nextInt();
+        int a[] = new int[n];
+        for (int i=0; i<n; i++){
+
+        }
+
+        System.out.println("Whoops");
+
+    }
+    public class Student{
+        String fname;
+        String lname;
+        double id;
+        double standing;
+        String email;
+        String address;
+        String city;
+        String state;
+        double zipcode;
+    }
+    public class getInfo{
+
+    }
+    public class displayInfo{
+    }
+}
+
